9baf51c909a2a9439b382e32b644dd98be270f57
SeakKimhour <seakkimhor20@kit.edu.kh>
2023-02-06 00:49:44 +0700

    working on ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+const appRoutes: Routes = [
+  { path: '', component: AuthPageComponent },
 
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth-page/auth-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit ,  ElementRef, VERSION, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-page',
+  templateUrl: './auth-page.component.html',
+  styleUrls: ['./auth-page.component.scss']
+})
+export class AuthPageComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  @ViewChild('container') container: ElementRef;
+
+  signIn() {
+    this.container.nativeElement.classList.remove('right-panel-active');
+  }
+
+  signUp() {
+    this.container.nativeElement.classList.add('right-panel-active');
+  }
+
+}
+
+
+
