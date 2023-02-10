af519ae1a88d13bb695b8b1729479a0f643d4e56
SrunChina <china.srunn@gmail.com>
2023-02-11 03:35:12 +0700

    frontend

--- src/app/services/guards/auth-guard.guard.ts
+++ src/app/services/guards/auth-guard.guard.ts
@@ -9,6 +9,7 @@ export class AuthGuardGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      // if user logged in
     if (localStorage.getItem('token') != null) {
       return true ;
     }
